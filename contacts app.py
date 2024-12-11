36737f019319f888b84421289a42809b97a43fb3
oskar015 <148869373+oskar015@users.noreply.github.com>
2024-12-11 23:54:39 +0000

    nested dictionary

---
+++ contacts app.py
@@ -0,0 +1,53 @@
+#A ‘contacts’ app that stores details (names, phone numbers, postal address etc),#  #
+# in which users can search via these details.#
+
+#welcome message
+print("This is your contacts app, here you can search for people in your contact list.")
+
+#a nested dictionary for the contact list [1], [2], [3]
+contacts = {
+  "Johnny Silverhand" : {
+    "phone number" : '0191 226 8885',
+    "postal address" : '18 Mitchcroft Road, Longstanton,CB24 3BE'
+  },
+  "Adam Smasher" : {
+    "phone number" : '07802 481946',
+    "postal address" : '10 Bispham Road, Nelson,BB9 0RX'
+  },
+  "Peter Griffin" : {
+    "phone number" : '01268 730955',
+    "postal address" : '56 Lampton Road, Hounslow,TW3 1JQ'
+  },
+  "Joe Swanson" : {
+    "phone number" : '01782 823035',
+    "postal address" : '6 Greenall Close, Cheshunt,EN8 9HY'
+  },
+  "Ichigo Kuroskai" : {
+    "phone number" : '01947 602664',
+    "postal address" : '31 Southgate Road, Southgate,SA3 2BY'
+  },
+  "Aizen Sosuke" : {
+    "phone number" : '0191 222 7662',
+    "postal address" : '1 Higher Devonshire Meadow, Grampound,TR2 4FS'
+  },
+  "Sasuke Uchiha" : {
+    "phone number" : '01903 237707',
+    "postal address" : '9 Castle Road, Cottingham,HU16 5JE'
+  },
+  "Kakashi Hatake" : {
+    "phone number" : '01603 766137',
+    "postal address" : '8 Fourview Close, Brixham,TQ5 9BR'
+  },
+  "Leonardo DiCaprio" : {
+    "phone number" : '01582 413704',
+    "postal address" : '126 Greenside Way, Walsall,WS5 4BH'
+  },
+  "Brad Pitt" : {
+    "phone number" : '01978 351815',
+    "postal address" : 'F1 The Lowry Designer Outlet The Quays, Salford,M50 3AH'
+  }
+}
+
+
+
+
