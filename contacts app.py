077efd668720652c3bb01c5d6b6cee8c79e35fb7
oskar015 <148869373+oskar015@users.noreply.github.com>
2024-12-12 23:47:05 +0000

    full code

--- contacts app.py
+++ contacts app.py
@@ -1,10 +1,28 @@
-#A ‘contacts’ app that stores details (names, phone numbers, postal address etc),#  #
-# in which users can search via these details.#
 
 #welcome message
-print("This is your contacts app, here you can search for people in your contact list.")
+print("Welcome to your contacts app.")
 
-#a nested dictionary for the contact list [1], [2], [3]
+#this function is for searching the dictionary to find a contact.
+#it searches by looping through the dictionary for the name of the contact [4], [1]
+def search_contact(contacts, name):
+  if name in contacts:
+    return contacts[name]
+  else:
+    return "No contact found."
+
+#this function is for adding a new contact, the user inputs details for the new contact [4]
+def add_contact(contacts):
+  name = input("Enter the contact's name: ")
+  phone_number = input("Enter the contact's phone number: ")
+  postal_address = input("Enter the contact's postal address: ")
+  #adding the new contact to the dictionary
+  contacts[name] = {
+    "phone number": phone_number,
+    "postal address": postal_address
+  }
+  print("Contact added.")
+
+#a nested dictionary for the contact list with each sub dictionary's name as key and values as phone number and postal code [1], [2], [3]
 contacts = {
   "Johnny Silverhand" : {
     "phone number" : '0191 226 8885',
@@ -22,7 +40,7 @@ contacts = {
     "phone number" : '01782 823035',
     "postal address" : '6 Greenall Close, Cheshunt,EN8 9HY'
   },
-  "Ichigo Kuroskai" : {
+  "Ichigo Kurosaki" : {
     "phone number" : '01947 602664',
     "postal address" : '31 Southgate Road, Southgate,SA3 2BY'
   },
@@ -48,6 +66,22 @@ contacts = {
   }
 }
 
-
-
-
+#the program loops until the user 'end's. This is so that the user can add a contact and then view it, or view more than one contact. [4]
+#based on user inputs, their 'choice' will determine the path of the program.
+while True:
+  choice = input("Here you can search for a contact or add a new one. (type search/add/end): ")
+  #searching the dictionary for the contact
+  if choice == 'search':
+    name = input("Enter the name of the contact you are searching for: ")
+    result = search_contact(contacts, name)
+    print(result)
+  #adding a contact to the dictionary
+  elif choice == 'add':
+    add_contact(contacts)
+  #ending the program.
+  elif choice == 'end':
+    print("program ended.")
+    break
+  #if the user doesn't type a valid choice.
+  else:
+    print("choose 'search', 'add', or 'end'.")
